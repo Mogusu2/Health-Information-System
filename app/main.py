21d602332a9f0700f156ae98d93b8ab1aa918158
James Maroko <jamiemaroko41@gmail.com>
2025-04-26 19:28:04 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,6 +1,15 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware  # Import CORSMiddleware
 from app.api import routes
 
 app = FastAPI(title="Health Information System")
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],  
+    allow_methods=["*"],  
+    allow_headers=["*"],  
+)
 
 app.include_router(routes.router)
